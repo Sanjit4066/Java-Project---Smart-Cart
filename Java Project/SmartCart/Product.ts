b9313c1959297018bb954b6104eeac655f4eb3b0
Sanjit4066 <133080514+Sanjit4066@users.noreply.github.com>
2025-11-25 22:16:56 +0530

    Add files via upload

---
+++ Java Project/SmartCart/Product.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  originalPrice?: number;
+  image: string;
+  category: string;
+  rating: number;
+  badge?: string;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
